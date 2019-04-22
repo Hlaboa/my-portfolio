0da9ce3496e07abfc3f8064c0fc3912365f46440
Hlaboa <hlaboa@gmail.com>
2019-04-22 09:03:04 +0000

    Add minor fixes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -119,20 +118,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static')
 ]
-# definimos el path que aparecerá en la barra de navegación
 STATIC_URL = '/static/'
-# definimos el directorio en nuestro proyecto (pueden ser nombres distintos)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # We specify where we want to save images/files/...
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 
 try:
     from .local_settings import *
